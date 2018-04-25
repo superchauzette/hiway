e373cc788a6b873bd43c098bb56c304dca331a42
kevin tillot <kevin.tillot@yellowinnovation.fr>
2018-04-25 10:45:05 +0200

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,5 +65,19 @@ module.exports = {
         skipWaiting: false,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-118126053-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
   ],
 }
