c42a6c3c34f137e8e201986cb898948e230e9c5c
kevin tillot <kevin.tillot@gmail.com>
2018-06-16 18:41:58 +0200

    remove gatsby-plugin-svgr

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,15 +7,6 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
-      resolve: 'gatsby-plugin-svgr',
-      options: {
-        icon: true,
-        viewBox: true,
-        // see https://github.com/smooth-code/svgr for a list of all options
-      },
-    },
-
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'Hiway',
