948d9e57d74b6bdb63b655927d24651088df3707
kevin tillot <kevin.tillot@gmail.com>
2018-06-17 00:11:09 +0200

    fix

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { ThemeProvider } from 'styled-components'
 import theme from './theme'
-
 import './custom.css'
 
 const TemplateWrapper = ({ children }) => (
