024db6791f1b52cd7f636db249323c4b9790b1ba
kevin tillot <kevin.tillot@gmail.com>
2018-09-22 15:58:47 +0200

    table responsive

--- src/components/ModesGestion.js
+++ src/components/ModesGestion.js
@@ -1,81 +1,122 @@
 import React from 'react'
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
+
 import { Box, Text, Heading1 } from '../components'
 import check from '../assets/check.svg'
 
 const TextImg = ({ text }) =>
-  text === undefined
-    ? <Text />
-    : <img width="25px" height="25px" src={text} />
-
-
-const Row = ({ text1, text2, text3, text4, text5 }) => (
-  <Box flexDirection="column" width="100%" height={["", "45px", "45px"]}>
-    <Box>
-      <Text width={["51%", "50%", "45%"]} pl={["5px", 0, "80px"]} fontWeight="bold">{text1}</Text>
-      <Box width="12%">
-        <TextImg text={text2} />
-      </Box>
-      <Box width="19%">
-        <TextImg text={text3} />
-      </Box>
-      <Box width="10%">
-        <TextImg text={text4} />
-      </Box>
-      <Box width="12%">
-        <Text>{text5}</Text>
-      </Box>
+  text === undefined ? (
+    <Text />
+  ) : (
+    <Box justifyContent="center">
+      <img width="25px" height="25px" src={text} />
     </Box>
-  </Box>
+  )
+
+const Row = ({ text }) => (
+  <Tr>
+    <Td>
+      <Text fontWeight="bold">
+        {text}
+      </Text>
+    </Td>
+    <Td>
+      <TextImg text={check} />
+    </Td>
+    <Td />
+    <Td>
+      <TextImg text={check} />
+    </Td>
+    <Td />
+  </Tr>
 )
 
 const ModesGestion = () => (
-  <Box bg="silver" width="100%" py={40} alignItems="center" flexDirection="column">
-
-    <Heading1 textAlign="center" px="10px">Choisis entre 2 modes de gestion</Heading1>
-
+  <Box
+    bg="silver"
+    width="100%"
+    py={40}
+    alignItems="center"
+    flexDirection="column"
+  >
+    <Heading1 textAlign="center" px="10px">
+      Choisis entre 2 modes de gestion
+    </Heading1>
 
     <Box width="100%" flexDirection="column">
-      <Box flexDirection="column" alignItems="flex-end" mr={40} textAlign="center">
-        <Box flexDirection="column" width={["90%", "100%", "60%"]}>
+      <Box
+        flexDirection="column"
+        alignItems="flex-end"
+        mr={40}
+        textAlign="center"
+      >
+        <Box flexDirection="column" width={['90%', '100%', '60%']}>
           <Box>
-            <Text width="44%" fontWeight="900" color="yellow" fontSize="1.2rem"
-            >Gestion déléguée à hiway</Text>
-            <Text width="50%" fontWeight="900" color="yellow" fontSize="1.2rem"> Gestion autonome</Text>
+            <Text width="44%" fontWeight="900" color="yellow" fontSize="1.2rem">
+              Gestion déléguée à hiway
+            </Text>
+            <Text width="50%" fontWeight="900" color="yellow" fontSize="1.2rem">
+              Gestion autonome
+            </Text>
           </Box>
         </Box>
 
-        <Box flexDirection="column" width={["100%", "100%", "60%"]} mr={["0px", "50px", "50px"]}>
+        <Box
+          flexDirection="column"
+          width={['100%', '100%', '60%']}
+          mr={['0px', '50px', '50px']}
+        >
           <Box>
-            <Text px={[0, "20px", "20px"]}>
-              Hiway révolutionne le freelancing en te libérant entièrement de la gestion de ton entreprise
+            <Text px={[0, '20px', '20px']}>
+              Hiway révolutionne le freelancing en te libérant entièrement de la
+              gestion de ton entreprise
             </Text>
-            <Text px={[0, "20px", "20px"]}>
-              Gère ton entreprise en autonomie avec le coaching et le support permanent d’Hiway
+            <Text px={[0, '20px', '20px']}>
+              Gère ton entreprise en autonomie avec le coaching et le support
+              permanent d’Hiway
             </Text>
           </Box>
         </Box>
       </Box>
-
     </Box>
 
-    <Box flexDirection="column" width="100%" height={["50px", "", ""]}>
-      <Box>
-        <Text width={["51%", "50%", "45%"]} pl={["5px", 0, "80px"]} color="lavender"></Text>
-        <Text width={["8%", "25%", "12%"]} fontWeight="bold" style={{overflowX: 'hidden'}}>hiway</Text>
-        <Text width={["12%", "25%", "19%"]}  fontWeight="bold" style={{overflowX: 'hidden'}}>Freelance</Text>
-        <Text width={["12%", "25%", "10%"]} fontWeight="bold" style={{overflowX: 'hidden'}}>Freelance</Text>
-        <Text width="12%" fontWeight="bold" style={{overflowX: 'hidden'}}>hiway</Text>
-      </Box>
+    <Box bg="silver" width="90%">
+      <Table>
+        <Thead>
+          <Tr>
+            <Th style={{ width: '40%' }} />
+            <Th><Text>Hiway</Text></Th>
+            <Th><Text>Freelance</Text></Th>
+            <Th><Text>Freelance</Text></Th>
+            <Th><Text>Hiway</Text></Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>
+              <Text fontWeight="bold">Édition du CRA</Text>
+            </Td>
+            <Td />
+            <Td>
+              <TextImg text={check} />
+            </Td>
+            <Td>
+              <TextImg text={check} />
+            </Td>
+            <Td />
+          </Tr>
+          <Row text="Contractualisation, facturation et relance client" />
+          <Row text="Traitement du courrier" />
+          <Row text="Gestion des obligations fiscales" />
+          <Row text="Gestion des obligations sociales" />
+          <Row text="Tenue de la comptabilité" />
+          <Row text="Calcul de la rémunération optimisée" />
+
+
+        </Tbody>
+      </Table>
     </Box>
-
-    <Row text1="Édition du CRA" text3={check} text4={check} text5="" />
-    <Row text1="Contractualisation, facturation et relance client" text2={check} text4={check} text5="" />
-    <Row text1="Traitement du courrier" text2={check} text4={check} />
-    <Row text1="Gestion des obligations fiscales" text2={check} text4={check} text5="" />
-    <Row text1="Gestion des obligations sociales" text2={check} text4={check} text5="" />
-    <Row text1="Tenue de la comptabilité" text2={check} text4={check} text5="" />
-    <Row text1="Calcul de la rémunération optimisée" text2={check} text4={check} />
-
   </Box>
 )
 
